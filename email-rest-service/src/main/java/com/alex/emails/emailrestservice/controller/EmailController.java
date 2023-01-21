059af4e36158d61a5dbe3d5b6986309433f7b1d6
alex_man <alexmnsbs@gmail.com>
2023-01-21 15:27:40 +0300

    add EmailController

---
+++ email-rest-service/src/main/java/com/alex/emails/emailrestservice/controller/EmailController.java
@@ -0,0 +1,75 @@
+package com.alex.emails.emailrestservice.controller;
+
+import com.alex.emails.emailrestservice.service.EmailService;
+import com.example.commondata.dto.EmailDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email")
+@Slf4j
+public class EmailController {
+
+    private final EmailService emailService;
+
+    /**
+     * Отправка письма на фиксированный mail
+     * @return
+     */
+    @PostMapping(value = "/send-fix-mail")
+    public ResponseEntity<?> sendFixMail(@RequestBody EmailDto emailDto) {
+        log.info("EmailController -- send-fix-mail -- start");
+        // man_angelina@mail.ru
+        emailService.sendFixMail("man_angelina@mail.ru",
+                "Test Subject", "Test mail");
+        log.info("Ушло");
+        return new ResponseEntity<>("Коллекция interview пустая", HttpStatus.OK);
+    }
+
+    /**
+     * Получен запрос на отправку письма на  mail
+     * @return
+     */
+    @PostMapping(value = "/sendmail")
+    public ResponseEntity<Object> sendMail(@RequestBody EmailDto emailDto) {
+        log.info("EmailController -- sendmail -- {}", emailDto);
+        if (emailDto != null) {
+            if (emailDto.getFullName() == null) {
+                return new ResponseEntity<>("Не заполнено поле с именем",
+                        HttpStatus.NOT_ACCEPTABLE);
+            }
+            if (emailDto.getTo() == null) {
+                return new ResponseEntity<>("Не заполнено поле email",
+                        HttpStatus.NOT_ACCEPTABLE);
+            }
+            if (emailDto.getSubject() == null) {
+                return new ResponseEntity<>("Не заполнено поле темы",
+                        HttpStatus.NOT_ACCEPTABLE);
+            }
+            if ((emailDto.getCode() == null) || (emailDto.getCode() == 0)) {
+                return new ResponseEntity<>("Нет кода подтвержения",
+                        HttpStatus.NOT_ACCEPTABLE);
+            }
+            log.info("Data OK");
+
+            if (emailService.sendMail(emailDto)) {
+                return new ResponseEntity<>("Сообщение отправлено", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Сообщение неотправлено",
+                        HttpStatus.REQUEST_TIMEOUT);
+            }
+        } else {
+            return new ResponseEntity<>("Объект EmailDto пустой", HttpStatus.NOT_ACCEPTABLE);
+
+        }
+
+    }
+}
