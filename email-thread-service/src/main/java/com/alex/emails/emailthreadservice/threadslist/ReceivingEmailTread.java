14cce2d8fcddeb14f322aacda21f1ff8e9d5f66e
alex_man <alexmnsbs@gmail.com>
2023-01-29 14:19:28 +0300

    add EmailVariable

--- email-thread-service/src/main/java/com/alex/emails/emailthreadservice/threadslist/ReceivingEmailTread.java
+++ email-thread-service/src/main/java/com/alex/emails/emailthreadservice/threadslist/ReceivingEmailTread.java
@@ -8,59 +8,11 @@ import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 
 import static com.example.commondata.data.EmailConstants.*;
+import static com.example.commondata.data.EmailVariable.emailList;
+import static com.example.commondata.data.EmailVariable.nameList;
 
 @Slf4j
 public class ReceivingEmailTread extends Thread {
-
-    // TODO: Переенсти в пакет с данными EmailVariable
-    private String[] nameList = {
-            "Emily",
-            "Hannah",
-            "Kaitlyn",
-            "Sarah",
-            "Madison",
-            "Brianna",
-            "Kaylee",
-            "Hailey",
-            "Alexis",
-            "Elizabeth",
-            "Taylor",
-            "Lauren",
-            "Ashley",
-            "Katherine",
-            "Jessica",
-            "Anna",
-            "Samantha",
-            "Alissa",
-            "Kayla",
-            "Madeline",
-            "Michael",
-            "James",
-            "Matthew",
-            "Nicholas",
-            "Christopher",
-            "Joseph",
-            "Zachary",
-            "Joshua",
-            "Andrew",
-            "William",
-            "Daniel",
-            "Christian",
-            "Tyler",
-            "Ryan",
-            "Anthony",
-            "Alexander",
-            "Jonathan",
-            "David",
-            "Dylan",
-            "Jacob"
-    };
-
-    // TODO: Переенсти в пакет с данными EmailVariable
-    private String[] emailList = {
-            "alex@gmail.com",
-            "lysan@gmail.com"
-    };
 
     private CommonTreadData commonTreadData;
 
